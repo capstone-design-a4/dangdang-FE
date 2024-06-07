4ebc79a65280f5ba73cac39aeec79e905f5b54be
gyozaa01 <olivia110321@gmail.com>
2024-06-07 16:16:57 +0900

    feature: 비밀번호 재설정 페이지 조건걸기

--- src/SearchPwForm.js
+++ src/SearchPwForm.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import Modal from './Modal';
 
 function SearchPwForm() {
     const [password, setPassword] = useState('');
@@ -9,9 +8,31 @@ function SearchPwForm() {
     const [showModal, setShowModal] = useState(false);
     const navigate = useNavigate();
 
+    const [errorModalMessage, setErrorModalMessage] = useState('');
+    const [showErrorModal, setShowErrorModal] = useState(false);
+
+
     const handlePasswordReset = async () => {
+        if (!password || !passwordCheck) {
+            setErrorModalMessage('비밀번호를 입력해주세요.');
+            setShowErrorModal(true);
+            return;
+        }
+    
+        if (password.length < 8 || password.length > 12 || !(/[a-zA-Z]/.test(password)) || !(/[0-9]/.test(password))) {
+            setErrorModalMessage('비밀번호는 8자에서 12자 사이이고, <br> 영문자와 숫자를 조합해야 합니다.');
+            setShowErrorModal(true);
+            return;
+        }
+    
+        if (password !== passwordCheck) {
+            setErrorModalMessage('비밀번호가 일치하지 않습니다.');
+            setShowErrorModal(true);
+            return;
+        }
+    
         try {
-            const response = await axios.post(`http://localhost:8080/api/member/setPassword`, null, {
+            const response = await axios.post('http://localhost:8080/api/member/setPassword', null, {
                 params: {
                     password: password,
                     passwordCheck: passwordCheck
@@ -20,26 +41,28 @@ function SearchPwForm() {
                     'accept': '*/*'
                 }
             });
-
+    
             if (response.data === "비밀번호가 재설정 되었습니다.") {
                 setShowModal(true);
             } else {
-                console.error('비밀번호 재설정 실패:', response.data);
+                setErrorModalMessage('비밀번호 재설정 실패: ' + response.data);
+                setShowErrorModal(true);
             }
         } catch (error) {
-            console.error('Error resetting password:', error);
+            setErrorModalMessage('Error resetting password: ' + error.message);
+            setShowErrorModal(true);
         }
-    };
+    };         
 
     const closeModal = () => {
         setShowModal(false);
         navigate('/mypage');
-    };
+    };   
 
     return (
         <div>
             <div className="sign">비밀번호 재설정</div>
-
+    
             <div className="form_box">
                 <div className="form">
                     <input
@@ -61,7 +84,7 @@ function SearchPwForm() {
                     비밀번호 재설정
                 </button>
             </div>
-
+    
             {showModal && (
                 <div className="modal">
                     <div className="modal_content">
@@ -70,8 +93,17 @@ function SearchPwForm() {
                     </div>
                 </div>
             )}
+    
+            {showErrorModal && (
+                <div className="error_modal">
+                    <div className="error_modal_content">
+                        <span className="close" onClick={() => setShowErrorModal(false)}>&times;</span>
+                        <div className="error-message" dangerouslySetInnerHTML={{__html: errorModalMessage}}></div>
+                    </div>
+                </div>
+            )}
         </div>
-    );
+    );    
 }
 
 export default SearchPwForm;
